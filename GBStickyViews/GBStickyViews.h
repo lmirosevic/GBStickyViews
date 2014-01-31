1e8494186a365e73d97478c6382a491d3ea38a7d
Luka Mirosevic <luka@goonbee.com>
2014-01-31 14:58:46 +0100

    Initial Commit

---
+++ GBStickyViews/GBStickyViews.h
@@ -0,0 +1,13 @@
+//
+//  GBStickyViews.h
+//  GBStickyViews
+//
+//  Created by Luka Mirosevic on 31/01/2014.
+//  Copyright (c) 2014 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBStickyViews : NSObject
+
+@end
